0efb293e9609a4f7ecf0acb4fe33df65336e8070
Márk Gyöngyösi <mrkgyngysi@gmail.com>
2023-09-12 19:44:54 +0200

    basic summary statistics with pandas

--- utils/summary_statistics.py
+++ utils/summary_statistics.py
@@ -1,2 +1,15 @@
-def summary_statistics():
-    return None
+# -*- coding: utf-8 -*-
+import pandas as pd
+
+df = pd.read_excel('../data/abalone.xls')
+df = df.drop('Sex', axis=1)
+    
+basic_summary_statistics = df.describe()
+print(basic_summary_statistics)
+    
+variance = df.var()
+print(variance)
+
+dfs = df.apply(lambda x: (x - x.mean()) / x.std())
+cov_matrix = dfs.cov()
+print(cov_matrix)
