7ca72f261b2533bcdd313680040d474e3e6a77fd
RasserAlt <rasser3500@gmail.com>
2023-10-02 20:34:37 +0200

    added sex to attributes in PCA via one out of K

--- main.py
+++ main.py
@@ -6,22 +6,24 @@ def main(file_name):
     # Load data
     # x0 is the first row of the data containing the discrete category variable Sex
     # y is the goal attribute we're interested in finding, here Rings
-    # X are the remanding continues variables
+    # X are the remanding continuous variables
     x0, X, y, variable_names, class_names = ut.load_xls(file_name)
 
     # Summary Statistics
     # ut.summary_statistics(file_name)
 
-    # Box plot for all continues attributes
-    ut.box_plot(X[:, :4], variable_names[1:5])
-    ut.box_plot(X[:, 4:], variable_names[5:8])
+    # Box plot for all continuous attributes
+    ut.box_plot(X[:, :4], variable_names[:4])
+    ut.box_plot(X[:, 4:], variable_names[4:])
 
     # Refactors the highest two height outliers by a factors of 10
     ut.refactor_outlier(X, 2, True, 10)
     ut.refactor_outlier(X, 2, True, 10)
 
     # Boxplot of refactored height
-    ut.box_plot(X[:, :4], variable_names[1:5])
+    ut.box_plot(X[:, :4], variable_names[:4])
+
+    X = np.concatenate((x0, X), axis=1)
 
     # Standardized table
     X_std = X - X.mean(axis=0)
@@ -30,15 +32,16 @@ def main(file_name):
     y_std = y - y.mean(axis=0)
     y_std = y_std / y_std.std(axis=0)
 
-    # Histogram of continues attributes
+    # Histogram of quantitative attributes
     Xy_std = np.concatenate((X_std, y_std), axis=1)
-    ut.hist_plot(Xy_std, variable_names[1:])
+    ut.hist_plot(Xy_std, variable_names+['ring'])
 
-    # Makes three different classes for the amount of rings
-    ring_class_names = ["[0:5]", "[5:10]", "[10:]"]
+    #categorizes the ring clas
+    ring_class = [0, 6, 12, 18]
+
 
     # PCA Analysis
-    ut.pca_analysis(X_std, y, ring_class_names, variable_names[1:8])
+    ut.pca_analysis(X_std, y, ring_class, class_names + variable_names)
 
 
 if __name__ == '__main__':
--- utils/pca_analysis.py
+++ utils/pca_analysis.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.linalg import svd
 
 
-def pca_analysis(X_std, y, class_names, variable_names):
+def pca_analysis(X_std, y, ring_class, variable_names):
     # PCA by computing SVD of Y
     U, S, Vh = svd(X_std, full_matrices=False)
 
@@ -26,8 +26,8 @@ def pca_analysis(X_std, y, class_names, variable_names):
 
     # From the plot we can see only two principal components are needed to explain 90% of the variance
     # So for projecting the data we use teo first indices of the principal components
-    i = 0
-    j = 1
+    i = 2
+    j = 3
 
     # of the vector V. So, for us to obtain the correct V, we transpose:
     V = Vh.T
@@ -39,13 +39,19 @@ def pca_analysis(X_std, y, class_names, variable_names):
     plt.figure()
     plt.title('Abalone data: PCA')
     y = y.flatten()
-
-    for c in range(len(class_names)):
+    ring_class_names = []
+    for c in range(len(ring_class)-1):
+        ring_class_names.append("[" + str(ring_class[c]) + ":" + str(ring_class[c + 1]) + "]")
         class_mask = []
         for h in range(len(y)):
-            class_mask.append(0 <= y[h] < 5 if c == 0 else 5 <= y[h] < 10 if c == 1 else 10 <= y[h])
-        plt.plot(Z[class_mask, i], Z[class_mask, j], '.', alpha=.75 - .25 * c)
-    plt.legend(class_names)
+            class_mask.append(ring_class[c] <= y[h] < ring_class[c+1])
+        plt.plot(Z[class_mask, i], Z[class_mask, j], '.', alpha=0.5)
+    ring_class_names.append("[" + str(ring_class[len(ring_class)-1]) + ":]")
+    class_mask = []
+    for h in range(len(y)):
+        class_mask.append(ring_class[len(ring_class)-1] <= y[h])
+    plt.plot(Z[class_mask, i], Z[class_mask, j], '.', alpha=0.5)
+    plt.legend(ring_class_names)
     plt.xlabel('PC{0}'.format(i + 1))
     plt.ylabel('PC{0}'.format(j + 1))
 
@@ -55,9 +61,9 @@ def pca_analysis(X_std, y, class_names, variable_names):
     # Plot to show the first two principal components coefficient on each continues attributes
     N, M = X_std.shape
 
-    pcs = [0, 1]
+    pcs = [0, 1, 2]
     legendStrs = ['PC' + str(e + 1) for e in pcs]
-    c = ['r', 'g']
+    c = ['r', 'g', 'b']
     bw = .2
     r = np.arange(1, M + 1)
     for i in pcs:
