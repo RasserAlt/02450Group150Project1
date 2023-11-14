c7bd3c26500f9c017830f7ddc5b1af9f14012046
Madis Otenurm <144136335+madioten@users.noreply.github.com>
2023-11-14 14:22:07 +0100

    Fix

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,6 +5,6 @@ from .plots import hist_plot, box_plot
 from .refactor_outlier import refactor_outlier
 from .regression import regularized_linear_regression
 from .lecture_tools import get_data_ranges, train_neural_net, draw_neural_net, visualize_decision_boundary
-from .ann import train_and_visualize_model, reduce_feature_space
+from .ann import train_and_visualize_model
 from .cross_validation import two_layer_cross_validation
 from .classification import classification
